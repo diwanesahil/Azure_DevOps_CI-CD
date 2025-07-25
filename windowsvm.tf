8b3e982459b8439ff1fa82c03e584dfead22af44
diwanesahil <dada.diwane001@gmail.com>
2025-07-25 22:08:51 +0530

    changed the name of VM

--- windowsvm.tf
+++ windowsvm.tf
@@ -12,7 +12,7 @@ resource "azurerm_network_interface" "windowsVMNIC" {
   
 }
 resource "azurerm_windows_virtual_machine" "windowsVM" {
-  name                = "WindowsVM"
+  name                = "WindowVM"
   resource_group_name = azurerm_resource_group.prodrg.name
   location            = azurerm_resource_group.prodrg.location
   size                = "Standard_D2s_v3"
@@ -69,5 +69,28 @@ resource "azurerm_public_ip" "windowsvmpip" {
   location            = azurerm_resource_group.prodrg.location
   resource_group_name = azurerm_resource_group.prodrg.name
   allocation_method   = "Static"
+  
+}
+
+resource "azurerm_public_ip" "natpip" {
+  name                = "NatPublicIP"
+  location            = azurerm_resource_group.prodrg.location
+  resource_group_name = azurerm_resource_group.prodrg.name
+  allocation_method   = "Static"
+  sku = "Standard"
+  
+}
+
+resource "azurerm_nat_gateway" "vmnatgw" {
+  name                = "WindowsVMNatGateway"
+  location            = azurerm_resource_group.prodrg.location
+  resource_group_name = azurerm_resource_group.prodrg.name
+  sku_name = "Standard"
+   
+}
+
+resource "azurerm_nat_gateway_public_ip_association" "natgtwpipassocuation" {
+  nat_gateway_id = azurerm_nat_gateway.vmnatgw.id
+  public_ip_address_id   = azurerm_public_ip.natpip.id
   
 }
