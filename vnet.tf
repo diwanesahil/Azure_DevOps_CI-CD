b3bdfeb64836b9e5ecd9f70682dfda062df7f234
diwanesahil <dada.diwane001@gmail.com>
2025-07-25 20:10:34 +0530

    modified windowsvm.tf

--- vnet.tf
+++ vnet.tf
@@ -1,4 +1,4 @@
-/*resource "azurerm_virtual_network" "ProductionVNet" {
+resource "azurerm_virtual_network" "ProductionVNet" {
   name                = "ProdVNet"
   resource_group_name = azurerm_resource_group.prodrg.name
   location            = azurerm_resource_group.prodrg.location
@@ -11,4 +11,4 @@ resource "azurerm_subnet" "ProductionSubnet" {
   resource_group_name  = azurerm_resource_group.prodrg.name
   virtual_network_name = azurerm_virtual_network.ProductionVNet.name
   address_prefixes     = ["10.0.0.0/24"]
-}*/
+}
