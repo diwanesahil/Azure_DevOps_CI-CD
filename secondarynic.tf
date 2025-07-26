a3fa928f3c0b906f9665afd11bdad15d7a5fe9ff
diwanesahil <dada.diwane001@gmail.com>
2025-07-26 18:35:18 +0530

    updated

--- secondarynic.tf
+++ secondarynic.tf
@@ -2,7 +2,7 @@ resource "azurerm_network_interface" "secondarynic" {
   name                = "SecondaryNIC"
   location            = azurerm_resource_group.prodrg.location
   resource_group_name = azurerm_resource_group.prodrg.name
-  depends_on = [ azurerm_public_ip.ipconfig1-pip, azurerm_public_ip.ipconfig2-pip, azurerm_public_ip.ipconfig3-pip, azurerm_public_ip.ipconfig4-pip, azurerm_public_ip.ipconfig5-pip    ]
+  
    
 
   ip_configuration {
@@ -10,15 +10,15 @@ resource "azurerm_network_interface" "secondarynic" {
     subnet_id                     = azurerm_subnet.ProductionSubnet.id
     private_ip_address_allocation = "Dynamic"
     primary = true
-    public_ip_address_id = azurerm_public_ip.ipconfig1-pip.id
     
+
       }
     ip_configuration {
     name                          = "ipconfig2"
     subnet_id                     = azurerm_subnet.ProductionSubnet.id
     private_ip_address_allocation = "Dynamic"
     primary = false
-    public_ip_address_id = azurerm_public_ip.ipconfig2-pip.id
+    
       
     }
 
@@ -27,7 +27,7 @@ resource "azurerm_network_interface" "secondarynic" {
     subnet_id                     = azurerm_subnet.ProductionSubnet.id
     private_ip_address_allocation = "Dynamic"
     primary = false
-    public_ip_address_id = azurerm_public_ip.ipconfig3-pip.id
+    
       
     }   
     ip_configuration {
@@ -35,7 +35,7 @@ resource "azurerm_network_interface" "secondarynic" {
     subnet_id                     = azurerm_subnet.ProductionSubnet.id
     private_ip_address_allocation = "Dynamic"
     primary = false
-    public_ip_address_id = azurerm_public_ip.ipconfig4-pip.id
+   
       
     }
     ip_configuration {
@@ -43,12 +43,13 @@ resource "azurerm_network_interface" "secondarynic" {
     subnet_id                     = azurerm_subnet.ProductionSubnet.id
     private_ip_address_allocation = "Dynamic"
     primary = false
-    public_ip_address_id = azurerm_public_ip.ipconfig5-pip.id
+    
       
     }  
 
   tags = {
     environment = "production"
   }
+  
   
 }
