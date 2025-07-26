625bf02ea52adaa810f2acaf471bc65cf9cba89e
diwanesahil <dada.diwane001@gmail.com>
2025-07-26 15:11:20 +0530

    fixed rg Issue

--- storage.tf
+++ storage.tf
@@ -1,11 +1,11 @@
 
 
-/*resource "azurerm_resource_group" "prodrg" {
+resource "azurerm_resource_group" "prodrg" {
   name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_storage_account" "demostorage" {
+/*resource "azurerm_storage_account" "demostorage" {
   name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.prodrg.name
   location                 = azurerm_resource_group.prodrg.location
